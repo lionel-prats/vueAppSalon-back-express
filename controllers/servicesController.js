b69287f06dff6395c676b83698f0f0b8c0837481
lionel-prats <lionel.prats.c@gmail.com>
2024-09-15 22:00:11 -0300

    Sección 32: PROYECTO - AppSalon - Servicios del Salón de Belleza - Finalizando Servicios en el Backend

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,6 +1,4 @@
-import { services } from "../data/beautyServices.js"
 import Services from "../models/Services.js"
-// import mongoose from "mongoose"
 import { validateObjectId, handleNotFoundError } from "../utils/index.js"
 
 
@@ -22,8 +20,13 @@ const createService = async (req, res) => {
     }
 }
 
-const getServices = (req, res) => {
-    res.json(services)
+const getServices = async (req, res) => {
+    try {
+        const services = await Services.find() 
+        return res.json(services)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const getServiceById = async (req, res) => {
--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -3,10 +3,18 @@ import { createService, getServices, getServiceById, updateService, deleteServic
 
 const router = express.Router()
 
-router.post("/", createService) // POST a http://localhost:4000/api/services
-router.get("/", getServices) // GET a http://localhost:4000/api/services
-router.get("/:id", getServiceById) // GET a http://localhost:4000/api/services/:id
-router.put("/:id", updateService) // PUT a http://localhost:4000/api/services/:id
-router.delete("/:id", deleteService) // DELETE a http://localhost:4000/api/services/:id
+// router.post("/", createService) // POST a http://localhost:4000/api/services
+// router.get("/", getServices) // GET a http://localhost:4000/api/services
+router.route("/")
+    .post(createService)
+    .get(getServices)
+
+// router.get("/:id", getServiceById) // GET a http://localhost:4000/api/services/:id
+// router.put("/:id", updateService) // PUT a http://localhost:4000/api/services/:id
+// router.delete("/:id", deleteService) // DELETE a http://localhost:4000/api/services/:id
+router.route("/:id")
+    .get(getServiceById)
+    .put(updateService)
+    .delete(deleteService)
 
 export default router
