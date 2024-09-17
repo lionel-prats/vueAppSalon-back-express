c7b011600212caa9656af1e7a7e555807d928395
lionel-prats <lionel.prats.c@gmail.com>
2024-09-16 22:55:47 -0300

    Sección 35: PROYECTO - AppSalon - Obtener y mostrar las Citas desde Nod, Express y Mongo via REST API

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 import express from "express" // ESM (b384)
 import dotenv from "dotenv" // (dependencia para acceder a las variables de entorno del .env) v389
 import colors from "colors" // v391
+import cors from "cors" // v417
 import { db } from "./config/db.js"
 import servicesRoutes from "./routes/servicesRoutes.js"
+
 
 // cargo las variobles de entorno (es como que este metodo de dotenv escanea las variables de entorno definidas en el .env) (v389)
 dotenv.config()
@@ -16,6 +18,25 @@ app.use(express.json())
 
 // conecto mi app al Cluster de MongoDB (v388)
 db()
+
+// Configurar CORS (v417)
+const whitelist =  [process.env.FRONTEND_URL] // lista blanca (dominios con acceso permitido a mis endpoints) (v417)
+if(process.argv[2] === "--postman") whitelist.push(undefined) // habilito poder seguir usando Postman y Thunder Client en desarrollo, no en produccion, asi que no hay peligro de que mi API quede expuesta (v417)
+const corsOptions = {
+    // origin -> nos da el dominio desde donde llega una peticion a cualquiera de mis endpoints
+    origin: function(origin, callback) {
+        if(whitelist.includes(origin)) {
+            // permitir la conexion (v417)
+            callback(null, true)
+        } else {
+            // NO permitir la conexion (v417)
+            callback(new Error("Error de CORS"))
+        }
+        // console.log(origin);
+    }
+}
+app.use(cors(corsOptions))
+// fin configurar CORS
 
 app.use("/api/services", servicesRoutes)
 // definir una ruta (v382)
